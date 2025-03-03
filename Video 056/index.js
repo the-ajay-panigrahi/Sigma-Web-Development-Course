1b04934625cbb8f3a5963ac9271fc0e2ccffd25a
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-03-03 18:45:19 +0530

    video 56 done

---
+++ Video 056/index.js
@@ -0,0 +1,25 @@
+let a = 2;
+let b = 3;
+
+console.log(a + b); // 5
+console.log(a - b); // -1
+console.log(a * b); // 6
+console.log(a / b); // 0.6666...
+console.log(a % b); // 2
+console.log(a ** b); // 8
+console.log(a > b); // false
+console.log(a < b); // true
+console.log(a >= b); // false
+console.log(a <= b); // true
+console.log(a == b); // false
+console.log(a === b); // false
+console.log(true && false); // false
+console.log(true || false); // true
+console.log(!false); // true
+
+let age = 21;
+if (age >= 18) {
+  console.log("You CAN vote!");
+} else {
+  console.log("You CANNOT vote!");
+}
